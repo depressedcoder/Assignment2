cd45d281ddb4022eb47088e379c2b72868c11da8
shantosiddiq <shanto.siddiq1@gmail.com>
2020-12-28 13:42:08 +0600

    Database

---
+++ CourseResult.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 28, 2020 at 07:40 AM
+-- Server version: 8.0.20
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `CourseResult`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `result`
+--
+
+CREATE TABLE `result` (
+  `Id` int NOT NULL,
+  `studentroll` varchar(100) NOT NULL,
+  `teacherId` varchar(50) NOT NULL,
+  `course1code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `course1classtest` double NOT NULL,
+  `course1final` double NOT NULL,
+  `course2code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `course2classtest` double NOT NULL,
+  `course2final` double NOT NULL,
+  `course3code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `course3classtest` double NOT NULL,
+  `course3final` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `result`
+--
+
+INSERT INTO `result` (`Id`, `studentroll`, `teacherId`, `course1code`, `course1classtest`, `course1final`, `course2code`, `course2classtest`, `course2final`, `course3code`, `course3classtest`, `course3final`) VALUES
+(2, '201119', '10258', '101', 35, 50, '102', 35, 50, '103', 35, 50),
+(3, '201119', '10312', '101', 30, 45, '102', 29, 45, '103', 35, 45),
+(4, '201108', '10312', '101', 20, 35, '102', 30, 35, '103', 25, 35);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `roll` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `resultsubmissioncount` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`roll`, `name`, `resultsubmissioncount`) VALUES
+('201108', 'Rahim', 1),
+('201117', 'Mobinur', 0),
+('201119', 'Farhad', 2),
+('201129', 'korim', 0),
+('201139', 'Zayed', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `teacherId` varchar(50) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(1000) NOT NULL,
+  `full_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `teacher`
+--
+
+INSERT INTO `teacher` (`teacherId`, `username`, `password`, `full_name`) VALUES
+('10312', 'rahman', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'Mr. Rahman'),
+('10258', 'saeed', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'Saeed Siddiq');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `result`
+--
+ALTER TABLE `result`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`roll`);
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `result`
+--
+ALTER TABLE `result`
+  MODIFY `Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
